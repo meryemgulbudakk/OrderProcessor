f25b5b6a22ba90b6f8090afbefb56a90daf436f4
meryemgulbudakk <meryem.gulbudakk@gmail.com>
2024-12-21 16:46:10 +0300

    Add entity classes for orders

---
+++ src/main/java/com/orderstream/orderprocessor/orders/model/Orders.java
@@ -0,0 +1,68 @@
+package com.orderstream.orderprocessor.orders.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+
+@Entity
+@Table(name = "orders")
+public class Orders {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderId;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id", nullable = false)
+    private Customer customer;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(nullable = false)
+    private LocalDateTime orderTimestamp;
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public LocalDateTime getOrderTimestamp() {
+        return orderTimestamp;
+    }
+
+    public void setOrderTimestamp(LocalDateTime orderTimestamp) {
+        this.orderTimestamp = orderTimestamp;
+    }
+}
